7cac62221ea3fb2d478099d519ecfb50a68b91b6
Jeet Kavthiya <jeetkavthiya21@gmail.com>
2022-12-30 23:13:06 +0530

    Day 1 Final

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,1 +1,39 @@
-console.log("Hello World");
+//console.log method
+console.log('Hello, World!');
+
+//console.log method with multiple parameter
+console.log('Hello', 'World', '!')
+console.log('HAPPY', 'NEW', 'YEAR', 2020)
+console.log('Welcome', 'to', 30, 'Days', 'Of', 'JavaScript')
+
+// This is the first comment
+// This is the second comment
+// I am a single line comment
+
+/*
+This is a multiline comment  
+ Multiline comments can take multiple lines  
+ JavaScript is the language of the web  
+*/
+
+//parameter with single quotes, double quotes and backticks
+console.log('Hello, World!')
+console.log("Hello, World!")
+console.log(`Hello, World!`)
+
+//Arithmetic Operations
+console.log(2 + 3)      // Addition
+console.log(3 - 2)      // Subtraction
+console.log(2 * 3)      // Multiplication
+console.log(3 / 2)      // Division
+console.log(3 % 2)      // Modulus - finding remainder
+console.log(3 ** 2)     // Exponentiation 3 ** 2 == 3 * 3
+
+//Datatypes
+let num = 10            // Number
+let name = "Jeet"       // String
+let male = true         // Boolean
+let ud = undefined      // Undefined
+let n = null            // Null
+let arr = []            // Array
+let obj = {}            // Object
